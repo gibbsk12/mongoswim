1bbde8e795b35fcdd52ef6754b7e6bb3a267369b
Kate Gibbs <kategibbs@Kates-MBP-2.fios-router.home>
2018-09-10 02:51:52 -0400

    handlebars kind of works

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var cheerio = require("cheerio");
 
 var Note = require("./models/Note");
 var Article = require("./models/Article");
-var PORT = 4040;
+var port = process.env.PORT || 4040;
 var app = express();
 
 app.use(logger("dev"));
@@ -16,7 +16,32 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/swimmingdb");
 
-app.get('/scrape', function (req, res) {
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Handlebars Renders
+app.get("/", function(req, res) {
+  Article.find({}, function(error, data) {
+    var hbsObject = {
+      article: data
+    };
+    console.log(hbsObject);
+    res.render("home", hbsObject);
+  });
+});
+
+app.get("/saved", function(req, res) {
+  Article.find({"saved": true}).populate("notes").exec(function(error, articles) {
+    var hbsObject = {
+      article: articles
+    };
+    res.render("saved", hbsObject);
+  });
+});
+
+app.get('/scrape', function (req, res) { //Scrapes the SwimSwam Site
   request("https://swimswam.com/news/", function (error, response, html) {
     var $ = cheerio.load(html);
     var results = {};
@@ -49,7 +74,7 @@ app.get('/scrape', function (req, res) {
   });
 })
 
-app.get("/articles", function(req, res){
+app.get("/articles", function(req, res){ //Grabs all articles 
   Article.deleteMany({ title: "" }, function (err) {    //Necessary to remove advertisements from db
     if (err) throw err;
   });
@@ -58,13 +83,95 @@ app.get("/articles", function(req, res){
     res.json(dbArticle);
   })
   .catch(function(err) {
-    // If an error occurred, send it to the client
     res.json(err);
   });
 })
 
-app.listen(PORT, function () {
-  console.log("App running on port " + PORT + "!");
+app.get("/articles/:id", function(req, res) { //Grabbing a specific article 
+  Article.findOne({ "_id": req.params.id })
+  .populate("note")
+  .exec(function(error, doc) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+app.post("/articles/save/:id", function(req, res) { //Saving an article
+  Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": true})
+  .exec(function(err, doc) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.send(doc);
+    }
+  });
+});
+
+app.post("/articles/delete/:id", function(req, res) { //Delete an article 
+  Article.findOneAndUpdate({ "_id": req.params.id }, {"saved": false, "notes": []})
+  .exec(function(err, doc) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.send(doc);
+    }
+  });
+});
+
+app.post("/notes/save/:id", function(req, res) { //Create a new note
+  var newNote = new Note({
+    body: req.body.text,
+    article: req.params.id
+  });
+  console.log(req.body)
+  newNote.save(function(error, note) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      Article.findOneAndUpdate({ "_id": req.params.id }, {$push: { "notes": note } })
+      .exec(function(err) {
+        if (err) {
+          console.log(err);
+          res.send(err);
+        }
+        else {
+          res.send(note);
+        }
+      });
+    }
+  });
+});
+
+app.delete("/notes/delete/:note_id/:article_id", function(req, res) { //Delete a Note
+  Note.findOneAndRemove({ "_id": req.params.note_id }, function(err) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    }
+    else {
+      Article.findOneAndUpdate({ "_id": req.params.article_id }, {$pull: {"notes": req.params.note_id}})
+        .exec(function(err) {
+          if (err) {
+            console.log(err);
+            res.send(err);
+          }
+          else {
+            res.send("Note Deleted");
+          }
+        });
+    }
+  });
+});
+
+app.listen(port, function () {
+  console.log("App running on port " + port + "!");
 });
 
 
